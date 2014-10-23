312d98748893150205fb85a0848024da6455f18a
chaugirl <chau.marialuisa@gmail.com>
2014-10-23 16:22:21 -0400

    Broke down the methods even further. (I think I was headed in the right direction though.)

--- mars_rover.rb
+++ mars_rover.rb
@@ -4,6 +4,7 @@
 # Rover's postion (2 integers and a letter separated by spaces) coordinates and orientation
 
 class Rover
+	attr_accessor :x, :y, :direction
 	def initialize(x, y, direction)
 		@x = x
 		@y = y
@@ -11,39 +12,48 @@ class Rover
 	end
 
 	# accept and instruction and decide whether to tell the rover to move or turn
-	def read_instruction ('move_turn_instr')
-		@move_turn_instr = move_turn_instr
+	# def read_instruction ('move_turn_instr')
+	# 	@move_turn_instr = move_turn_instr
 
-		#break up the string into individual letters.
-		@move_turn_instr.each_char { |direction| 
-		if direction == N && instr == L
-			direction is now west
-		elsif direction == N && instr == R
-			direction is now east
-		elsif direction == E && instr == L
-			direction is now N
-		elsif direction == E && instr == R
-			direction is now S
-		elsif direction == S && instr == L
-			direction is now E
-		elsif direction == S && instr == R
-			direction is now west
-		elsif direction == W && instr == L
-			direction is now S
-		elsif direction == W && instr == R
-			direction is now N
-		else
-			@x++
-		}
+	# 	#break up the string into individual letters.
+	# 	@move_turn_instr.each_char { |direction| 
+	# 	if direction == N && instr == L
+	# 		direction is now west
+	# 	elsif direction == N && instr == R
+	# 		direction is now east
+	# 	elsif direction == E && instr == L
+	# 		direction is now N
+	# 	elsif direction == E && instr == R
+	# 		direction is now S
+	# 	elsif direction == S && instr == L
+	# 		direction is now E
+	# 	elsif direction == S && instr == R
+	# 		direction is now west
+	# 	elsif direction == W && instr == L
+	# 		direction is now S
+	# 	elsif direction == W && instr == R
+	# 		direction is now N
+	# 	else
+	# 		@x++
+	# 	}
+	# end
+	def read_instruction(instruction)
+		if instruction == "M"
+			move
+		elsif instruction == "L"
+			turn_left
+		elsif instruction == "R"
+			turn_right
+		end
 	end
-
-	# affects the position of the rover and updates coordinates if necessary
-	def move
-		
-	end
-
-	# affects the direction of the rover; either L or R
-	def turn
-		
-	end
+		def move
+			if @direction == "N"
+				@y += 1
+			end	
+		end	
 end
+
+rover = Rover.new(0, 0, "N")
+puts "Rover's current position is #{rover.x}, #{rover.y}, facing #{rover.direction}"
+rover.read_instruction("M")
+puts "Rover's current position is #{rover.x}, #{rover.y}, facing #{rover.direction}"
