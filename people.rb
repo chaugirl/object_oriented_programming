0f579667b0aa91fb056556e1c58d4f5fcdc57182
chaugirl <chau.marialuisa@gmail.com>
2014-10-23 14:14:37 -0400

    Added greeting method to super class Person

--- people.rb
+++ people.rb
@@ -2,19 +2,21 @@ class Person
 	def initialize(name)
 		@name = name
 	end
+	def greeting
+		puts "Hi, my name is #{@name}"
+	end
 end
 
-class Student
+class Student < Person
 	def learn
 		puts "I get it"
 	end
 end
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby has an Object"
-	end
-	
+	end	
 end
 
 
