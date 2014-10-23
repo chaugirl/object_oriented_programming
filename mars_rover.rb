7246ba3327785391b8f8576fbdd8d661ed9fb62f
chaugirl <chau.marialuisa@gmail.com>
2014-10-23 15:53:26 -0400

    Trying to work out the read instructions method in my terms (as opposed to ruby terms)

--- mars_rover.rb
+++ mars_rover.rb
@@ -11,8 +11,30 @@ class Rover
 	end
 
 	# accept and instruction and decide whether to tell the rover to move or turn
-	def read_instruction 
-	
+	def read_instruction ('move_turn_instr')
+		@move_turn_instr = move_turn_instr
+
+		#break up the string into individual letters.
+		@move_turn_instr.each_char { |direction| 
+		if direction == N && instr == L
+			direction is now west
+		elsif direction == N && instr == R
+			direction is now east
+		elsif direction == E && instr == L
+			direction is now N
+		elsif direction == E && instr == R
+			direction is now S
+		elsif direction == S && instr == L
+			direction is now E
+		elsif direction == S && instr == R
+			direction is now west
+		elsif direction == W && instr == L
+			direction is now S
+		elsif direction == W && instr == R
+			direction is now N
+		else
+			@x++
+		}
 	end
 
 	# affects the position of the rover and updates coordinates if necessary
